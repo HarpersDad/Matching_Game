6cc6327473a9531a8bb496f7cb541adaf7113841
Clifton Lindsey <42348662+HarpersDad@users.noreply.github.com>
2023-09-21 11:08:29 -0400

    Add files via upload

--- Match/src/Functions.java
+++ Match/src/Functions.java
@@ -1,3 +1,4 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.util.Arrays;
@@ -11,15 +12,20 @@ public class Functions
     static int scoreCount = 0;
     static JButton button1 = null;
     static JButton button2 = null;
+    static String cardPath = "";
+    static String cardPathA = "";
+    static String cardPathB = "";
 
     // create and set Button bounds
     public static void setButtonBounds(JFrame myFrame, JButton myButton, int x, int y, int w, int h)
     {
-        myButton.setFont(new Font("Arial", Font.PLAIN, 12));
+        myButton.setFont(new Font("Arial", Font.PLAIN, 0));
         myButton.setBounds(x, y, w, h);
         myButton.setForeground(Color.BLACK);
         myButton.setBackground(Color.BLUE);
         myFrame.getContentPane().add(myButton);
+
+        setBackImage(myButton);
     }
 
     // create and set character data labels
@@ -29,6 +35,83 @@ public class Functions
         myLabel.setBounds(x, y, w, h);
         myLabel.setForeground(Color.BLACK);
         myFrame.getContentPane().add(myLabel);
+    }
+
+    // sets the button image
+    public static void setBackImage(JButton myButton)
+    {
+        try
+        {
+            Image myImage = ImageIO.read(Objects.requireNonNull(Functions.class.getResource("Resources/cardBack.png")));
+            myButton.setIcon(new ImageIcon(myImage));
+        }
+        catch(Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void setFrontImage(JButton myButton)
+    {
+        getCard(myButton);
+
+        myButton.setIcon(null);
+
+        try
+        {
+            Image myImage = ImageIO.read(Objects.requireNonNull(Functions.class.getResource(cardPath)));
+            myButton.setIcon(new ImageIcon(myImage));
+        }
+        catch(Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void getCard(JButton myButton)
+    {
+        switch (myButton.getText())
+        {
+            case "2":
+                cardPath = "Resources/cards/2.png";
+                break;
+            case "3":
+                cardPath = "Resources/cards/3.png";
+                break;
+            case "4":
+                cardPath = "Resources/cards/4.png";
+                break;
+            case "5":
+                cardPath = "Resources/cards/5.png";
+                break;
+            case "6":
+                cardPath = "Resources/cards/6.png";
+                break;
+            case "7":
+                cardPath = "Resources/cards/7.png";
+                break;
+            case "8":
+                cardPath = "Resources/cards/8.png";
+                break;
+            case "9":
+                cardPath = "Resources/cards/9.png";
+                break;
+            case "10":
+                cardPath = "Resources/cards/10.png";
+                break;
+            case "J":
+                cardPath = "Resources/cards/J.png";
+                break;
+            case "Q":
+                cardPath = "Resources/cards/Q.png";
+                break;
+            case "K":
+                cardPath = "Resources/cards/K.png";
+                break;
+            case "A":
+                cardPath = "Resources/cards/A.png";
+                break;
+        }
     }
 
     // sets button1 and button2
@@ -41,14 +124,31 @@ public class Functions
 
             // sets the button text
             setButtonText(button1);
+
+            // sets button image
+            setFrontImage(button1);
+
+            button1.setText(null);
+
+            // sets the card's image path for reference
+            cardPathA = cardPath;
         }
         else
         {
             button2 = myButton;
             button2.setBackground(Color.WHITE);
+            button2.setIcon(null);
 
             // sets the button text
             setButtonText(button2);
+
+            // sets button image
+            setFrontImage(button2);
+
+            button2.setText(null);
+
+            // sets the card's image path for reference
+            cardPathB = cardPath;
 
             // checks if the two "cards" match
             checkMatch();
@@ -58,7 +158,7 @@ public class Functions
     // checks to see if the two chosen cards match
     public static void checkMatch()
     {
-        if (Objects.equals(button1.getText(), button2.getText()))
+        if (Objects.equals(cardPathA, cardPathB))
         {
             UI.myFrame.remove(button1);
             UI.myFrame.remove(button2);
@@ -69,8 +169,6 @@ public class Functions
             scoreCount++;
 
             UI.scoreL.setText("Score: " + scoreCount);
-
-            System.out.println(UI.score);
 
             UI.myFrame.repaint();
         }
@@ -83,6 +181,9 @@ public class Functions
 
             button1.setBackground(Color.BLUE);
             button2.setBackground(Color.BLUE);
+
+            setBackImage(button1);
+            setBackImage(button2);
 
             button1 = null;
             button2 = null;
@@ -100,12 +201,9 @@ public class Functions
     }
 
     // sets the passed button's text to that of corresponding letter at the same array position
-    public static void setButtonText(JButton thisButton)
-    {
-        for (int i = 0; i < UI.buttons.length; i++)
-        {
-            if (thisButton == UI.buttons[i])
-            {
+    public static void setButtonText(JButton thisButton) {
+        for (int i = 0; i < UI.buttons.length; i++) {
+            if (thisButton == UI.buttons[i]) {
                 thisButton.setText(UI.buttonText[i]);
             }
         }
--- Match/src/UI.java
+++ Match/src/UI.java
@@ -6,11 +6,12 @@ public class UI
     // set and initialize variables
     static int y = 800;
     static int x = 1040;
-    static int width = 175;
-    static int height = 300;
-    static int bottomRow = y - height - 75;
-    static int topRow = y - (2*height) - 100;
-    static int score = 0;
+    static int width = 120;
+    static int height = 168;
+    static int lowerBottomRow = y - height - 45;
+    static int upperBottomRow = y - (2*height) - 55;
+    static int lowerTopRow = y - (3*height) - 65;
+    static int upperTopRow = y - (4*height) - 75;
     static JFrame myFrame = new JFrame();
     static JButton buttonA = new JButton();
     static JButton buttonB = new JButton();
@@ -22,9 +23,28 @@ public class UI
     static JButton buttonH = new JButton();
     static JButton buttonI = new JButton();
     static JButton buttonJ = new JButton();
+    static JButton buttonK = new JButton();
+    static JButton buttonL = new JButton();
+    static JButton buttonM = new JButton();
+    static JButton buttonN = new JButton();
+    static JButton buttonO = new JButton();
+    static JButton buttonP = new JButton();
+    static JButton buttonQ = new JButton();
+    static JButton buttonR = new JButton();
+    static JButton buttonS = new JButton();
+    static JButton buttonT = new JButton();
+    static JButton buttonU = new JButton();
+    static JButton buttonV = new JButton();
+    static JButton buttonW = new JButton();
+    static JButton buttonX = new JButton();
+    static JButton buttonY = new JButton();
+    static JButton buttonZ = new JButton();
     static JLabel scoreL = new JLabel("Score: 0");
-    static String[] buttonText = {"A", "B", "C", "D", "E", "A", "B", "C", "D", "E"};
-    static Object[] buttons = {buttonA, buttonB, buttonC, buttonD, buttonE, buttonF, buttonG, buttonH, buttonI, buttonJ};
+    static String[] buttonText = {"A", "A", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7",
+            "8", "8", "9", "9", "10", "10", "J", "J", "Q", "Q", "K", "K"};
+    static Object[] buttons = {buttonA, buttonB, buttonC, buttonD, buttonE, buttonF, buttonG, buttonH, buttonI,
+            buttonJ, buttonK, buttonL, buttonM, buttonN, buttonO, buttonP, buttonQ, buttonR, buttonS, buttonT,
+            buttonU, buttonV, buttonW, buttonX, buttonY, buttonZ};
 
     // function that creates the UI
     static void createUI()
@@ -33,23 +53,43 @@ public class UI
         myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         myFrame.setSize(x, y);
         myFrame.setLocationRelativeTo(null);
-        myFrame.getContentPane().setBackground(Color.WHITE);
+        myFrame.getContentPane().setBackground(Color.GRAY);
 
         // function call that rearranges the buttonText array
         Functions.shuffleAndSet(buttonText);
 
         // function calls that create the buttons
-        Functions.setButtonBounds(myFrame, buttonA, 25, topRow, width, height);
-        Functions.setButtonBounds(myFrame, buttonB, 25, bottomRow, width, height);
-        Functions.setButtonBounds(myFrame, buttonC, 225, topRow, width, height);
-        Functions.setButtonBounds(myFrame, buttonD, 225, bottomRow, width, height);
-        Functions.setButtonBounds(myFrame, buttonE, 425, topRow, width, height);
-        Functions.setButtonBounds(myFrame, buttonF, 425, bottomRow, width, height);
-        Functions.setButtonBounds(myFrame, buttonG, 625, topRow, width, height);
-        Functions.setButtonBounds(myFrame, buttonH, 625, bottomRow, width, height);
-        Functions.setButtonBounds(myFrame, buttonI, 825, topRow, width, height);
-        Functions.setButtonBounds(myFrame, buttonJ, 825, bottomRow, width, height);
-        Functions.setLabelBounds(myFrame, scoreL, 25, topRow - 50, 50, 20);
+        Functions.setButtonBounds(myFrame, buttonA, 120, upperTopRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonB, 250, upperTopRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonC, 380, upperTopRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonD, 510, upperTopRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonE, 640, upperTopRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonF, 770, upperTopRow, width, height);
+
+        Functions.setButtonBounds(myFrame, buttonG, 55, lowerTopRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonH, 185, lowerTopRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonI, 315, lowerTopRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonJ, 445, lowerTopRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonK, 575, lowerTopRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonL, 705, lowerTopRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonM, 835, lowerTopRow, width, height);
+
+        Functions.setButtonBounds(myFrame, buttonN, 55, upperBottomRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonO, 185, upperBottomRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonP, 315, upperBottomRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonQ, 445, upperBottomRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonR, 575, upperBottomRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonS, 705, upperBottomRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonT, 835, upperBottomRow, width, height);
+
+        Functions.setButtonBounds(myFrame, buttonU, 120, lowerBottomRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonV, 250, lowerBottomRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonW, 380, lowerBottomRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonX, 510, lowerBottomRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonY, 640, lowerBottomRow, width, height);
+        Functions.setButtonBounds(myFrame, buttonZ, 770, lowerBottomRow, width, height);
+
+        Functions.setLabelBounds(myFrame, scoreL, 25, upperTopRow - 25, 100, 20);
 
         myFrame.setResizable(false);
         myFrame.setLayout(null);
@@ -66,5 +106,21 @@ public class UI
         buttonH.addActionListener(e -> Functions.checkClick(buttonH));
         buttonI.addActionListener(e -> Functions.checkClick(buttonI));
         buttonJ.addActionListener(e -> Functions.checkClick(buttonJ));
+        buttonK.addActionListener(e -> Functions.checkClick(buttonK));
+        buttonL.addActionListener(e -> Functions.checkClick(buttonL));
+        buttonM.addActionListener(e -> Functions.checkClick(buttonM));
+        buttonN.addActionListener(e -> Functions.checkClick(buttonN));
+        buttonO.addActionListener(e -> Functions.checkClick(buttonO));
+        buttonP.addActionListener(e -> Functions.checkClick(buttonP));
+        buttonQ.addActionListener(e -> Functions.checkClick(buttonQ));
+        buttonR.addActionListener(e -> Functions.checkClick(buttonR));
+        buttonS.addActionListener(e -> Functions.checkClick(buttonS));
+        buttonT.addActionListener(e -> Functions.checkClick(buttonT));
+        buttonU.addActionListener(e -> Functions.checkClick(buttonU));
+        buttonV.addActionListener(e -> Functions.checkClick(buttonV));
+        buttonW.addActionListener(e -> Functions.checkClick(buttonW));
+        buttonX.addActionListener(e -> Functions.checkClick(buttonX));
+        buttonY.addActionListener(e -> Functions.checkClick(buttonY));
+        buttonZ.addActionListener(e -> Functions.checkClick(buttonZ));
     }
 }
